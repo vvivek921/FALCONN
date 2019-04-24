3df9774c34fa8a12f59bd3edaf57d9a579a10499
vvivek921 <vsrivivek9+blr@gmail.com>
2019-04-24 15:47:58 +0530

    mark data as dense

--- src/examples/glove/glove_sparse.cc
+++ src/examples/glove/glove_sparse.cc
@@ -326,7 +326,7 @@ int main() {
         = get_default_parameters<Point>(dataset.size(),
                                    300,
                                    DistanceFunction::NegativeInnerProduct,
-                                   false);
+                                   true);
     cout << "building the index based on the cross-polytope LSH" << endl;
     t1 = high_resolution_clock::now();
     auto table = construct_table<Point>(dataset, params);
