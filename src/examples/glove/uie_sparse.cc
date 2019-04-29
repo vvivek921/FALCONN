b2d7aeb73960ddc8dcf06fad7a82deec9ed5885c
vvivek921 <vsrivivek9+blr@gmail.com>
2019-04-29 12:12:53 +0530

    default params

--- src/examples/glove/uie_sparse.cc
+++ src/examples/glove/uie_sparse.cc
@@ -381,28 +381,28 @@ int main() {
    // params.storage_hash_table = StorageHashTable::BitPackedFlatHashTable;
    ///   For an easy way out, you could have used the following.
 
-//    LSHConstructionParameters params
-//      = get_default_parameters<Point>(dataset.size(),
-//                                 972,
-//                                 DistanceFunction::NegativeInnerProduct,
-//                                 true);
-//    cout << "building the index based on the cross-polytope LSH" << endl;
+    LSHConstructionParameters params
+      = get_default_parameters<Point>(dataset.size(),
+                                 972,
+                                 DistanceFunction::NegativeInnerProduct,
+                                 false);
+    cout << "building the index based on the cross-polytope LSH" << endl;
 //    params.feature_hashing_dimension=256;
 
-    LSHConstructionParameters params;
-    params.dimension = 972;
-    params.lsh_family = LSHFamily::Hyperplane;
-    params.l = NUM_HASH_TABLES;
-    params.distance_function = DistanceFunction::EuclideanSquared;   
-    //params.distance_function = DistanceFunction::NegativeInnerProduct;
-compute_number_of_hash_functions<Point>(NUM_HASH_BITS, &params);
-    params.num_rotations = NUM_ROTATIONS;
-    // we want to use all the available threads to set up
-    params.num_setup_threads = 0;
-    params.storage_hash_table = StorageHashTable::BitPackedFlatHashTable;
-    params.feature_hashing_dimension = 128;
-
-
+//    LSHConstructionParameters params;
+//    params.dimension = 972;
+//    params.lsh_family = LSHFamily::Hyperplane;
+//    params.l = NUM_HASH_TABLES;
+//    params.distance_function = DistanceFunction::EuclideanSquared;
+//    //params.distance_function = DistanceFunction::NegativeInnerProduct;
+//compute_number_of_hash_functions<Point>(NUM_HASH_BITS, &params);
+//    params.num_rotations = NUM_ROTATIONS;
+//    // we want to use all the available threads to set up
+//    params.num_setup_threads = 0;
+//    params.storage_hash_table = StorageHashTable::BitPackedFlatHashTable;
+//    params.feature_hashing_dimension = 128;
+//
+//
     t1 = high_resolution_clock::now();
     auto table = construct_table<Point>(dataset, params);
     t2 = high_resolution_clock::now();
