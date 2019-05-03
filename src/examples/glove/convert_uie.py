ec2025dd3fe0b1ed54dec68177fc978447bbe8c2
vvivek921 <vsrivivek9+blr@gmail.com>
2019-05-03 08:02:30 +0530

    convert uie

---
+++ src/examples/glove/convert_uie.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+import struct
+from itertools import islice
+
+with open('/mnt/vivek-data2.csv', 'r') as inf:
+    with open('/mnt/uie-sparse.dat', 'wb') as ouf:
+        try:
+            counter = 0
+            while True:
+                data =  islice(inf, 0, 3)
+                le = next(data)
+                idx = next(data)
+                data = next(data)
+                ouf.write(struct.pack('i', le))
+                ouf.write(struct.pack('%si' % le , *idx ))
+                ouf.write(struct.pack('%sf' % le,*data ))
+                counter+=3
+                if counter % 10000 == 0:
+                    sys.stdout.write('%d points processed...\n' % counter)
+        except StopIteration:
+            pass
