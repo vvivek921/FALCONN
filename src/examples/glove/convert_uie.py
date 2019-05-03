23fda2e15759f436a0d00f78ae82b66ebbcd15e5
vvivek921 <vsrivivek9+blr@gmail.com>
2019-05-03 08:09:02 +0530

    str to int

--- src/examples/glove/convert_uie.py
+++ src/examples/glove/convert_uie.py
@@ -11,8 +11,8 @@ with open('/mnt/vivek-data2.csv', 'r') as inf:
             while True:
                 data =  islice(inf, 0, 3)
                 le = int(next(data))
-                idx = [int(i) for i in next(data)]
-                data= [float(i) for i in next(data)]
+                idx = [int(i) for i in next(data).split(",")]
+                data= [float(i) for i in next(data).split(",")]
                 ouf.write(struct.pack('i', le))
                 ouf.write(struct.pack('%si' % le , *idx ))
                 ouf.write(struct.pack('%sf' % le,*data ))
